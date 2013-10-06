37fd010cd0d5f143c66ccbabbb161c55f094a925
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-10-06 10:48:51 -0400

    syncing properties from package.json to bower.json automatically

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,16 @@
 /*global module:false*/
 module.exports = function (grunt) {
-  // time tasks
   module.require('time-grunt')(grunt);
 
   grunt.initConfig({
+
+    sync: {
+      all: {
+        options: {
+          sync: ['author', 'name', 'version', 'description', 'homepage', 'license']
+        }
+      }
+    },
 
     jshint: grunt.file.readJSON('jshint.json'),
 
@@ -25,5 +32,6 @@ module.exports = function (grunt) {
   var plugins = module.require('matchdep').filterDev('grunt-*');
   plugins.forEach(grunt.loadNpmTasks);
 
-  grunt.registerTask('default', ['deps-ok', 'jshint', 'node-qunit', 'qunit', 'complexity']);
+  grunt.registerTask('default', ['deps-ok', 'sync',
+    'jshint', 'node-qunit', 'qunit', 'complexity']);
 };
